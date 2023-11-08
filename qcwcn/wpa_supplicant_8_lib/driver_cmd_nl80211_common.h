d21966c138ae7da07286df5fa1035599714de12f
Shailendra Singh <quic_shasing@quicinc.com>
2023-11-08 19:00:21 +0530

    wpa_supplicant_8_lib: Add command to get / set ML link config
    Add host driver command to set / get Multi Link config
    which provides info of link id and link state.
    To get link config: driver GET_ML_LINK_CONTROL_MODE
    To set link config: driver SET_ML_LINK_CONTROL_MODE config_mode <mode>
    [link_id <id> link_state <state>..]
    
    Change-Id: Icf9ff70b44e37be32ae1940d68bf3dbfd058105a
    CRs-Fixed: 3673081

--- qcwcn/wpa_supplicant_8_lib/driver_cmd_nl80211_common.h
+++ qcwcn/wpa_supplicant_8_lib/driver_cmd_nl80211_common.h
@@ -51,9 +51,11 @@
 
 #define OBSS_PD_THRESHOLD_MIN -82
 #define OBSS_PD_THRESHOLD_MAX -62
+#define NL80211_ATTR_MAX_INTERNAL 256
 
 struct wpa_driver_nl80211_data *drv;
 struct i802_bss *bss;
+
 struct nl_msg *prepare_vendor_nlmsg(struct wpa_driver_nl80211_data *drv,
 		                    char *ifname, int subcmd);
 
@@ -69,3 +71,5 @@ int wpa_driver_sr_event(struct wpa_driver_nl80211_data *drv,
 char *skip_white_space(char *cmd);
 char *get_next_arg(char *cmd);
 s32 get_s32_from_string(char *cmd_string, int *ret);
+char *move_to_next_str(char *cmd);
+u8 get_u8_from_string(char *cmd_string, int *ret);
